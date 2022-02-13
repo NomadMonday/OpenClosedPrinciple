ec5fca07d48713d7632b9b05505f49e27a75ad89
NomadMonday <NomadMonday@gmail.com>
2022-02-12 22:42:03 -0800

    Simplify ApplicantModel by not requiring abstract properties for names. Enforce use of Accounts abstract base class by adding a class attribute AccountsProcessor which is used to create the accounts_processor instance attribute.

--- End/applicants/person_model.py
+++ End/applicants/person_model.py
@@ -2,27 +2,4 @@ from applicants.abc_applicant_model import ApplicantModel
 from accounts.staff_accounts import StaffAccounts
 
 class PersonModel(ApplicantModel):
-    def __init__(self, first_name, last_name):
-        self._first_name = first_name
-        self._last_name = last_name
-        self._account_processor = StaffAccounts()
-    
-    @property
-    def first_name(self):
-        return self._first_name
-
-    @first_name.setter
-    def first_name(self, value):
-        self._first_name = value
-
-    @property
-    def last_name(self):
-        return self._last_name
-
-    @last_name.setter
-    def last_name(self, value):
-        self._last_name = value
-
-    @property
-    def account_processor(self):
-        return self._account_processor
+    AccountProcessor = StaffAccounts
